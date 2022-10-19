2fc87ce40070e0cd8c6284c36ce11870f595bc26
senS24t <116205383+senS24t@users.noreply.github.com>
2022-10-19 21:28:41 +0300

    Add files via upload

---
+++ sbr.lua
@@ -0,0 +1,77 @@
+if game.PlaceId == 4643697430 then
+wait(1)
+    local runServ = game:GetService("RunService")
+if workspace:FindFirstChild("Message") then workspace.Message.Text:match("COMMENCING")
+    repeat wait() print("waitforstar") until workspace:FindFirstChild("Message")==nil
+end
+if workspace:FindFirstChild("Message")==nil then
+    print("a")
+    repeat wait(1)
+    for i,v in ipairs(game:GetService("Workspace").Barriers:GetChildren()) do
+        if v:IsA("Part") and v.Name == ("1") then game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = v.CFrame 
+        print('aas')
+end
+        end
+until game:GetService("Workspace").Barriers:FindFirstChild("1")==nil
+
+if workspace:FindFirstChild("Message")==nil then
+    print("a")
+    repeat wait(1)
+    for i,v in ipairs(game:GetService("Workspace").Barriers:GetChildren()) do
+        if v:IsA("Part") and v:FindFirstChild("1")==nil then if v.Name == ("2") then game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = v.CFrame 
+        print('aas')
+end
+        end
+    end
+until game:GetService("Workspace").Barriers:FindFirstChild("2")==nil
+end
+
+if workspace:FindFirstChild("Message")==nil then
+    print("a")
+    repeat wait(1)
+    for i,v in ipairs(game:GetService("Workspace").Barriers:GetChildren()) do
+        if v:IsA("Part") and v:FindFirstChild("2")== nil then if v.Name == ("3") then game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = v.CFrame 
+        print('aas')
+end
+        end
+    end
+until game:GetService("Workspace").Barriers:FindFirstChild("3")==nil
+end
+
+if workspace:FindFirstChild("Message")==nil then
+    print("a")
+    repeat wait(1)
+    for i,v in ipairs(game:GetService("Workspace").Barriers:GetChildren()) do
+        if v:IsA("Part") and v:FindFirstChild("3")== nil then if v.Name == ("4") then game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = v.CFrame 
+        print('aas')
+end
+        end
+    end
+until game:GetService("Workspace").Barriers:FindFirstChild("4")==nil
+end
+
+print("stage 4")
+
+end
+
+if workspace:WaitForChild("Message") then if workspace.Message.Text:match("stage #4") then
+    repeat wait()
+        game:GetService("Workspace").Living.Madaraa1246.HumanoidRootPart.CFrame = CFrame.new(-1335.88, 385.636, 7913.82)
+        print("waitinga")
+        game:GetService("Workspace").Living.Madaraa1246.HumanoidRootPart.CFrame = CFrame.new(-1350.88, 380.636, 7813.82)
+        if workspace:WaitForChild("Message").Text:match("There's now") then
+        end
+    until workspace:WaitForChild("Message").Text:match("There's now")
+end
+end
+    if workspace:WaitForChild("Message").Text:match("There's now") then
+        repeat wait()
+        runServ:BindToRenderStep("dafrt", Enum.RenderPriority.Camera.Value, function()
+        game:GetService("Workspace").Living.Madaraa1246.HumanoidRootPart.CFrame = CFrame.new(-1235.88, 812.636, 7813.82)
+        game:GetService("Workspace").Living.Madaraa1246.HumanoidRootPart.CFrame = CFrame.new(-1235.88, 512.636, 7813.82)
+if workspace:WaitForChild("Message")==nil then
+end
+    end)
+until workspace:WaitForChild("Message")==nil
+end
+end
